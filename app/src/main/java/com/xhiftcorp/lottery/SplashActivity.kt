83aef57bf26bd88fc15441f0316f7a90fafef9df
spiderman <wonderful.coding.life@gmail.com>
2020-09-16 18:16:33 +0900

    did some code refactoring

--- app/src/main/java/com/xhiftcorp/lottery/SplashActivity.kt
+++ app/src/main/java/com/xhiftcorp/lottery/SplashActivity.kt
@@ -1,32 +1,33 @@
 package com.xhiftcorp.lottery
 
 import android.content.Intent
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
-    private var finished: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        animationView.setOnClickListener {
-            finished = true
+        val runnable = Runnable {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
+        val handler = Handler(Looper.getMainLooper());
+        handler.postDelayed(runnable, 3000)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            if (!finished) {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
-            }
-        }, 3000)
+        animationView.setOnClickListener {
+            handler.removeCallbacks(runnable)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
